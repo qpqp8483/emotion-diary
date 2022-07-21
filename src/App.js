68e9637b8048d8ee9e573e2393aaa951d4b1d156
chanwoo <qpqp8483@naver.com>
2022-07-21 17:26:17 +0900

    edit App,index,DiaryList,Home

--- src/App.js
+++ src/App.js
@@ -35,9 +35,42 @@ const reducer = (state, action) => {
 export const DiaryStateContext = React.createContext();
 export const DiaryDispatchContext = React.createContext();
 
+const dummyData = [
+  {
+    id: 1,
+    emotion: 1,
+    content: '오늘의 일기 1번',
+    date: 1658388246708
+  },
+  {
+    id: 2,
+    emotion: 2,
+    content: '오늘의 일기 2번',
+    date: 1658388246709
+  },
+  {
+    id: 3,
+    emotion: 3,
+    content: '오늘의 일기 3번',
+    date: 1658388246710
+  },
+  {
+    id: 4,
+    emotion: 4,
+    content: '오늘의 일기 4번',
+    date: 1658388246711
+  },
+  {
+    id: 5,
+    emotion: 5,
+    content: '오늘의 일기 5번',
+    date: 1658388246712
+  },
+]
+
 function App() {
 
-  const [data, dispatch] = useReducer(reducer, []);
+  const [data, dispatch] = useReducer(reducer, dummyData);
   const dataId = useRef();
 
   //CREATE
---
+++ src/components/DiaryList.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+
+const sortoOptionList = [
+    { value: 'lastest', name: '최신 순' },
+    { value: 'oldest', name: '오래된 순' }
+]
+
+const ControlMenu = ({ value, onChange, optionList }) => {
+    return (
+        <select value={value} onChange={(e)=> onChange(e.target.value)} >
+            {optionList.map((it, idx)=>(
+                <option key={idx} value={it.value}>
+                    {it.name}
+                </option>
+            ))} 
+        </select>
+    )
+}
+
+const DiaryList = ({ diaryList }) => {
+
+    const [sortType, setSortType]  = useState('lastest');
+
+    const ProcessedDiaryList = () => {
+        const compare = (a, b) => {
+            if(sortType === 'lastest'){
+                return parseInt(b.date)  - parseInt(a.date);
+            }else{
+                return parseInt(a.date)  - parseInt(b.date);
+            }
+        }
+
+        const copyList = JSON.parse(JSON.stringify(diaryList));
+        const sortedList = copyList.sort(compare);
+        return sortedList
+    }
+
+    return (
+        <div>
+            <ControlMenu
+                value={sortType}
+                onChange={setSortType}
+                optionList={sortoOptionList}
+            />
+            {ProcessedDiaryList().map((it)=>(
+                <div key={it.id}>
+                    {it.content}
+                </div>
+            ))}
+        </div>
+    );
+};
+
+DiaryList.defaultProps = {
+    diaryList: [],
+}
+
+export default DiaryList;
